1b3cbe1269a802241901d12152c476fd276a4991
Natan <natan.alvarenga@ufu.br>
2022-11-03 10:40:22 -0300

    somaGauss.c

--- somaGauss.c
+++ somaGauss.c
@@ -6,4 +6,16 @@
 */
 
 #include <stdio.h>
-int main
+int main(void){
+    int n, soma, i;
+    printf("Digite o numero: ");
+    scanf("%d", &n);
+    soma = 0;
+    i = 1;
+    while(i <= n){
+    soma = soma + i;
+    i = i + 1;
+    }
+    printf("A soma e: %d", soma);
+    return 0;
+}
