c4c9ec10c8d62125ea37bccd38edf944b0180e68
Natan <natan.alvarenga@ufu.br>
2022-11-04 10:44:09 -0300

    somaGauss.c

--- somaGauss.c
+++ somaGauss.c
@@ -13,8 +13,8 @@ int main(void){
     soma = 0;
     i = 1;
     while(i <= n){
-    soma = soma + i;
-    i = i + 1;
+        soma = soma + i;
+        i = i + 1;
     }
     printf("A soma e: %d", soma);
     return 0;
