0107e62102674b2a2c80e3e903987b09d03f7634
Natan <natan.alvarenga@ufu.br>
2022-11-04 10:58:51 -0300

    fatorial.c

---
+++ fatorial.c
@@ -0,0 +1,24 @@
+/*
+*Arquivo: fatorial.c
+*Data criação: 04/nov/22
+*Autor: Natan Ferreira
+*Meu primeiro programa em C
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+
+int main(void){
+    int n, i;
+    int64_t produtorio;
+    produtorio = 1;
+    i = 1;
+    printf("Digite um numero maior que 1: ");
+    scanf("%d", &n);
+    while (i <= n){
+        produtorio = (produtorio * i);
+        i = i + 1;
+    }
+    printf("O fatorial de %d e: %d", n, produtorio);
+    return 0;
+}
