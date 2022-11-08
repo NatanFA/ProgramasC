b3198e378bb9f609722821d7e7f3215f1f51e127
Natan <natan.alvarenga@ufu.br>
2022-11-07 21:35:01 -0300

    fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -7,10 +7,9 @@
 
 #include <stdio.h>
 int main(void){
+    int a;
     printf("Digite quantos numeros da sequencia de fibonacci deseja: ");
-    scanf("%d", &n);
-    for (int i = 0, j = 1, t = i + j; j < n; i = j; j = t; t = i + j){
-        printf("%d", i);
-    }
+    scanf("%d", &a);
+    for (int i = 0, j = 0, k = 1, z; i < a; i++, z = j+k, j = k, k = z) printf("%d\n", j);
     return 0;
 }
