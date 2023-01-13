03b81fd9c807a743bd08c1fa70a8faed4f31e169
Natan <natan.alvarenga@ufu.br>
2023-01-13 09:31:57 -0300

    LabPP

--- Teste.c
+++ Teste.c
@@ -2,9 +2,8 @@
 #include <string.h>
 
 int main(void){
-    char s[12];
-    char n[12];
-    scanf("%s %s", &s, &n);
-    printf("%s %s", s, n);
+    float a = 2;
+    int b = 3;
+    printf("%f", a/b);
     return 0;
 }
